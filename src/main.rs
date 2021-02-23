8fd3fa250fb7726a4d5530e1ed4a654f284e3f69
Kevin Marker <kmarker@gmail.com>
2021-02-23 08:27:12 -0600

    I think this will be ok?

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
     println!("Hello, world!");
     println!("Just a test for Github actions to Main!");
-    
+    let needle = 0xCB;
+    let haystack = [1, 1, 2, 5, 15, 52, 203, 877, 4140, 21147];
+
+    for item in haystack.iter() {
+        if *item == needle {
+            println!("{}", item);
+        }
+    }
 }
